94f475c85bb863bcbdb681327793eefaad02c2d6
Divya Ranjan Behera <divyarbehera@gmail.com>
2018-06-20 19:50:30 +0530

    set up files

---
+++ src/app/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Header extends React.Component{
+render(){return(
+    <div class="container-fluid">
+                <p>Header</p>
+    </div>
+);}
+};
---
+++ src/app/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Home extends React.Component{
+render(){return(
+    <div class="container-fluid">
+                <p>Home</p>
+    </div>
+);}
+};
